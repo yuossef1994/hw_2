b8afbdcef4a8b59932e937d4179da40b220ed867
yuossef1994 <abouhussieny@vcu.edu>
2023-02-24 15:25:13 -0800

    Initial Commit

---
+++ hw_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw_2
+//
+//  Created by Youssef Z on 2/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
